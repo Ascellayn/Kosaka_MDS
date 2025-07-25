420748d418fe38a711c74ced2cdefb3220555a8e
Ascellayn <root.siriusb@gmail.com>
2025-07-25 16:01:36 +0200

    Missing type safety on Root_CFG global variable

--- KMDS/Globals.py
+++ KMDS/Globals.py
@@ -1,7 +1,7 @@
 from TSN_Abstracter import *;
 import asyncio, flask, httpx, json, re, time, threading, yt_dlp;
 
-Root_CFG = File.JSON_Read("Root_CFG.json");
+Root_CFG: dict = File.JSON_Read("Root_CFG.json");
 Debug_Mode: bool = Root_CFG["Debug"];
 KMDS_Version: str = Root_CFG["Version"];
 
