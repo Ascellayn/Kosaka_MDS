2904906da2694f12dd80b1fcef4d5bce0446ee23
Ascellayn <root.siriusb@gmail.com>
2025-08-02 22:50:04 +0200

    [v1.1.3] oops bad regex

--- KMDS/Globals.py
+++ KMDS/Globals.py
@@ -1,7 +1,7 @@
 from TSN_Abstracter import *;
 import asyncio, flask, httpx, json, os, re, time, threading, yt_dlp;
 
-KMDS_Version: str = "v1.1.2";
+KMDS_Version: str = "v1.1.3";
 
 Root_CFG: dict = File.JSON_Read("Root_CFG.json");
 Debug_Mode: bool = Root_CFG["Debug"];
